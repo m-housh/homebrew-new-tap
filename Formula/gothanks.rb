0ca1380ce6e2e12c5799dac9cdb91c609befa926
Michael Housh <9305678+m-housh@users.noreply.github.com>
2022-02-15 00:23:51 +0000

    gothanks: add 0.3.0 bottle.

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -5,6 +5,12 @@ class Gothanks < Formula
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/m-housh/homebrew-new-tap/releases/download/gothanks-0.3.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "fcffa756d980a008ec3728bfe54998a505372cdd44cce830e2ab006f7b314ec3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "dba7fa11acf159b13b1ca9e398000748bb16381ba0aadc8aeb1f6883107ef34c"
+  end
+
   depends_on "go" => :build
 
   def install
